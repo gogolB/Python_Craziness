db7d31cb1acdf0310859e5494f4e874a7f429dd6
gogolB <sbhat006@ucr.edu>
2016-01-03 19:37:23 -0800

    Done with Chapter 2.
    All code works with Anaconda 2.4.1 and Python 3.5.1

--- Machine Learning in Action/Chapter 2 - k Nearest Neighbors/kNN.py
+++ Machine Learning in Action/Chapter 2 - k Nearest Neighbors/kNN.py
@@ -1,5 +1,6 @@
 from numpy import *
 import operator
+from os import listdir
 
 # Creates a test Dataset.
 def createDataSet():
@@ -77,3 +78,40 @@ def classifyPerson():
     inArr = array([ffmiles, percentTats, iceCream]);
     classifierResult = classify0((inArr-minVals)/ranges, normMat, datingLabels, 3);
     print("You will probably like this person: %s" % (classifierResult));
+
+# Converts an 32 x 32 image to a vector.
+def img2vector(filename):
+    returnVect = zeros((1,1024));
+    fr = open(filename);
+    for i in range(32):
+        lineStr = fr.readline();
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j]);
+    return returnVect;
+
+# Unit test for handwriting checking.
+def handwritingClassTest():
+    hwLabels = [];
+    trainingFileList = listdir('trainingDigits');
+    m = len(trainingFileList);
+    trainingMat = zeros((m, 1024));
+    for i in range(m):
+        fileNameStr = trainingFileList[i];
+        fileStr = fileNameStr.split('.')[0];
+        classNumStr = int(fileStr.split('_')[0]);
+        hwLabels.append(classNumStr);
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr);
+    testFileList = listdir('testDigits');
+    errorCount = 0.0;
+    mTest = len(testFileList);
+    for i in range(mTest):
+        fileNameStr = testFileList[i];
+        fileStr = fileNameStr.split('.')[0];
+        classNumStr = int(fileStr.split('_')[0]);
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr);
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3);
+        print("The classifier came back with %d, the real answer is %d" % (classifierResult, classNumStr));
+        if(classifierResult != classNumStr) : errorCount += 1;
+
+    print("The total number of errors is: %d" % errorCount);
+    print("The total error rate is: %f" % (errorCount/float(mTest)));
